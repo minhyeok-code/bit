d0acddaae7d2172e2385b99a6763d080afad5dfa
choiminhyeok <edum96@gmail.com>
2025-12-03 13:42:08 +0900

    modifing

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 실행 중인 프로세스 종료
-pkill -f todoApp-0.0.1-SNAPSHOT.jar || echo "No app running"
+pkill -f bitcoinlion-0.0.1-SNAPSHOT.jar || echo "No app running"
 
 # 프로세스 종료 대기
 sleep 15
